8a1e345816134cde198d3835eadb28b21b5f90d1
Gabriel de Oliveira Ferreira <38558405+gabrielferreirah@users.noreply.github.com>
2018-05-03 11:28:05 -0300

    CEDUP - 03/05

---
+++ Exercicio01.java
@@ -0,0 +1,22 @@
+public class Exercicio01{
+    
+    public static void main(String[] args) {
+
+        int[] numeros = new int[16];
+        
+        numeros[0] = 1;
+        numeros[1] = 2;
+        numeros[2] = 3;
+        numeros[3] = 4;
+        numeros[4] = 5;
+        numeros[5] = 6;
+        numeros[6] = 7;
+        numeros[7] = 8;
+        numeros[8] = 9;
+        numeros[9] = 10;
+        numeros[10] = 11;
+        numeros[11] = 12;
+        numeros[12] = 13;
+        
+    }
+}
