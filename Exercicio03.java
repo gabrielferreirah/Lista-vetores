1d3a21de7512255ef8df5ad86a1d266248b7d018
Gabriel de Oliveira Ferreira <gabrieloferreira.u4@gmail.com>
2018-05-04 02:26:45 -0300

    CEDUP 04/05

---
+++ Exercicio03.java
@@ -0,0 +1,14 @@
+public class Exercicio03{
+
+    private static DecimalFormat df2 = new DecimalFormat(".##");
+    
+    public static void main(String[] args) {
+        
+        DecimalFormat
+        String[] nomes = new String[7];
+        double[] precos = new double[7];
+        
+        nomes[1] = "Teclado Mecanico";
+        precos[1] = 150;
+    }
+}
